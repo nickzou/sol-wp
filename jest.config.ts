6643b407cb9fa0f34a9139e3771703d29de5e99c
Nick Zou <nick@elevate.ca>
2023-09-01 22:05:13 -0400

    add babel-jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import { JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
 
 export default config;
