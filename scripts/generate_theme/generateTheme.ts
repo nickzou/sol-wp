622b01bb375b2736964710e5d643dc0371bad84b
Nick Zou <nick@elevate.ca>
2023-09-02 01:23:42 -0400

    add ts version of the generateTheme script

--- scripts/generate_theme/generateTheme.ts
+++ scripts/generate_theme/generateTheme.ts
@@ -1,7 +1,9 @@
+import getDescription from "./getDescription/getDescription";
 import getThemeName from "./getThemeName/getThemeName";
 
 const generateTheme = async () => {
   const themeName = await getThemeName();
+  const description = await getDescription();
 };
 
 (async () => {
