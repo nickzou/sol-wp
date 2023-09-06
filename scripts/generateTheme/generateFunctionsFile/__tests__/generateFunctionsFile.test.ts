73aef7c47dff60e4912138ee199912a81763b586
Nick Zou <nick@elevate.ca>
2023-09-06 18:49:40 -0400

    fix formatting

--- scripts/generateTheme/generateFunctionsFile/__tests__/generateFunctionsFile.test.ts
+++ scripts/generateTheme/generateFunctionsFile/__tests__/generateFunctionsFile.test.ts
@@ -4,7 +4,7 @@ describe("generateFunctionsFile", () => {
   it("should return a File object with the correct name and content", () => {
     const expectedContent = `<?php
   //Functions go here;
-  ?>`;
+?>`;
     const result = generateFunctionsFile();
 
     expect(result.name).toBe("functions.php");
--- scripts/generateTheme/generateFunctionsFile/generateFunctionsFile.ts
+++ scripts/generateTheme/generateFunctionsFile/generateFunctionsFile.ts
@@ -3,7 +3,7 @@ import { File } from "../../utils/types/File";
 const generateFunctionsFile = (): File => {
   const content = `<?php
   //Functions go here;
-  ?>`;
+?>`;
 
   return {
     name: "functions.php",
