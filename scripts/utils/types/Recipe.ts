bfb9c1b984c7183e609ea60fc0a88f3f5afc9210
Nick Zou <nick@elevate.ca>
2023-09-07 16:31:10 -0400

    add Recipe type

---
+++ scripts/utils/types/Recipe.ts
@@ -0,0 +1,14 @@
+export type Recipe = {
+  theme: {
+    name: string;
+    folder: string;
+    author: string;
+    description: string;
+    version: string;
+  };
+  setUpTooling: boolean;
+  tooling: {
+    css: "tailwind" | "unocss" | "postcss" | "sass" | "none";
+    ts: boolean;
+  };
+};
