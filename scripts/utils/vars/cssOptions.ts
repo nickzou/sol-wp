f8ad1df414070f3c495541237d2c167c58ce6b35
Nick Zou <nick@elevate.ca>
2023-09-09 20:09:24 -0400

    change packageName from dart-sass to sass

--- scripts/utils/vars/cssOptions.ts
+++ scripts/utils/vars/cssOptions.ts
@@ -3,7 +3,7 @@ import { CssOption } from "../types/CssOption";
 const cssOptions: CssOption[] = [
   { name: "tailwind", packageName: "tailwindcss" },
   { name: "uno", packageName: "unocss" },
-  { name: "sass", packageName: "dart-sass" },
+  { name: "sass", packageName: "sass" },
   { name: "postcss", packageName: "postcss" },
   { name: "css", packageName: null },
   { name: "none", packageName: null },
