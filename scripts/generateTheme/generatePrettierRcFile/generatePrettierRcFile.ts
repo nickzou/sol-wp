488e12eb56632b22c3f04653ead5d3a7b0f6bbe3
Nick Zou <nick@elevate.ca>
2023-09-10 00:24:56 -0400

    add generatePrettierRcFile function

---
+++ scripts/generateTheme/generatePrettierRcFile/generatePrettierRcFile.ts
@@ -0,0 +1,20 @@
+import { File } from '@utils/types/File';
+
+const generatePrettierRcFile = (): File => {
+  const content = `{
+    "semi": true,
+    "trailingComma": "all",
+    "singleQuote": true,
+    "printWidth": 80,
+    "plugins": [
+    ]
+  };
+  `;
+
+  return {
+    name: '.prettierrc',
+    content,
+  };
+};
+
+export default generatePrettierRcFile;
