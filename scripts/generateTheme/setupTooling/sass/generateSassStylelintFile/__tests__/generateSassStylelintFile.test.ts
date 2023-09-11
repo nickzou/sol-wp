372adcd0687d751f3ebdc5f55668753dbe368329
Nick Zou <nick@elevate.ca>
2023-09-10 21:06:27 -0400

    fix tests

---
+++ scripts/generateTheme/setupTooling/sass/generateSassStylelintFile/__tests__/generateSassStylelintFile.test.ts
@@ -0,0 +1,16 @@
+import generateSassStylelintFile from '@generateTheme/setupTooling/sass/generateSassStylelintFile/generateSassStylelintFile';
+
+describe('generateSassStylelintFile', () => {
+  it('should return the correct File object', () => {
+    const expectedFile = {
+      name: '.stylelintrc.json',
+      content: `{
+  "extends": ["stylelint-config-standard-scss"]
+}`
+    };
+
+    const generatedFile = generateSassStylelintFile();
+
+    expect(generatedFile).toEqual(expectedFile);
+  });
+});
