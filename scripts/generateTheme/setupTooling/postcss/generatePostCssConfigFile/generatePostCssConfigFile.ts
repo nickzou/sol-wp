dccc901d62051c05f411ae32c9227794af178ccf
Nick Zou <nick@elevate.ca>
2023-09-11 21:48:10 -0400

    fix whitespace

--- scripts/generateTheme/setupTooling/postcss/generatePostCssConfigFile/generatePostCssConfigFile.ts
+++ scripts/generateTheme/setupTooling/postcss/generatePostCssConfigFile/generatePostCssConfigFile.ts
@@ -1,23 +1,23 @@
 import { File } from "@utils/types/File";
 
 const generatePostCssConfigFile = ():File => {
-  const content =`import autoprefixer from 'autoprefixer';
-  import autoReset  from 'postcss-autoreset';
-  import nested from 'postcss-nested';
-  import cssnano from 'cssnano';
+  const content =`import * as autoprefixer from 'autoprefixer';
+import autoReset  from 'postcss-autoreset';
+import nested from 'postcss-nested';
+import cssnano from 'cssnano';
   
-  const config = {
-    plugins: [
-      autoprefixer,
-      autoReset,
-      nested,
-      cssnano({
-        preset: 'default',
-      }),
-    ],
-  };
+const config = {
+  plugins: [
+    autoprefixer,
+    autoReset,
+    nested,
+    cssnano({
+      preset: 'default',
+    }),
+  ],
+};
   
-  export default config;`;
+export default config;`;
   
   return {
     name: 'postcss.config.ts',
