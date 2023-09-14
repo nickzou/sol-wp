bf32fcd495515510fb2935e19d94fe7e11d09a07
Nick Zou <nick@elevate.ca>
2023-09-14 18:40:10 -0400

    fix typo

--- scripts/generateTheme/setupTooling/postcss/generatePostCssConfigFile/generatePostCssConfigFile.ts
+++ scripts/generateTheme/setupTooling/postcss/generatePostCssConfigFile/generatePostCssConfigFile.ts
@@ -4,7 +4,7 @@ const generatePostCssConfigFile = (): File => {
   const content = `{
   "plugins": {
     "autoprefixer": {},
-    "postcess-nested": {},
+    "postcss-nested": {},
     "postcss-autoreset": {
       "reset": {
         "*": {
