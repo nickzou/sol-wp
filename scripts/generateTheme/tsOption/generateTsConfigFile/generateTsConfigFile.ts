94079f8308115144acd8a01daca9a019066fde96
Nick Zou <nick@elevate.ca>
2023-09-15 00:04:39 -0400

    add more configs to tsconfig template

--- scripts/generateTheme/tsOption/generateTsConfigFile/generateTsConfigFile.ts
+++ scripts/generateTheme/tsOption/generateTsConfigFile/generateTsConfigFile.ts
@@ -1,7 +1,7 @@
 import { File } from "@utils/types/File";
 
 const generateTsConfigFile = (): File => {
-  const content = `{\n  "compilerOptions": {\n    "module": "NodeNext",\n   "moduleResolution": "NodeNext",\n   "target": "ES6",\n    "esModuleInterop": true,\n    "noEmit": true\n  }\n}`;
+  const content = `{\n  "compilerOptions": {\n    "esModuleInterop": true,\n    "forceConsistentCasingInFileNames":true,\n    "skipLibCheck":true,\n   "target": "ES6",\n   "verbatimModuleSyntax":true,\n    "allowJs":true,\n   "resolveJsonModule":true,\n   "moduleDetection":"force",\n    "strict":true,\n    "noUncheckedIndexedAccess":true,\n    "module": "NodeNext",\n   "moduleResolution": "Bundler",\n    "noEmit": true,\n   "lib":["es2022","dom"],\n  }\n}`;
   return {
     name: "tsconfig.json",
     content,
