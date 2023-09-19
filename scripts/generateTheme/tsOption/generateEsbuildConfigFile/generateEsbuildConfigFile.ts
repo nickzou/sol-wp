34b1a1a3e11bcc7e2d4dd7e7dd3a0ac3ad43e849
Nick Zou <nick@elevate.ca>
2023-09-18 21:01:11 -0400

    add types

--- scripts/generateTheme/tsOption/generateEsbuildConfigFile/generateEsbuildConfigFile.ts
+++ scripts/generateTheme/tsOption/generateEsbuildConfigFile/generateEsbuildConfigFile.ts
@@ -12,18 +12,19 @@ import glob from 'glob';
 import browserslist from 'browserslist';
 import { esbuildPluginBrowserslist } from 'esbuild-plugin-browserslist';
 import { green, red } from 'colorette';
+import { type Format } from 'esbuild';
   
 const filesFlag = process.argv.filter(a => a.includes("--files"));
 const minifyFlag = process.argv.filter(a => a.includes("--minify"));
 const sourcemapFlag = process.argv.filter(a => a.includes("--sourcemap"));
 const formatFlag = process.argv.filter(a => a.includes("--format"));
   
-const fileDir = filesFlag.length >= 1 ? filesFlag[0]!.split("=")[1] : "./src/ts/*.{ts,tsx}";
+const fileDir = filesFlag.length >= 1 ? filesFlag[0]!.split("=")[1] as string : "./src/ts/*.{ts,tsx}";
   
 const minify = minifyFlag.length >= 1 || (minifyFlag.length >= 1 && minifyFlag[0]!.split("=")[1] === "true") ? true : false;
 const sourcemap = sourcemapFlag.length >= 1 || (sourcemapFlag.length >= 1 && sourcemapFlag[0]!.split("=")[1]) === "true" ? true : false; 
   
-const format = formatFlag.length >=1 ? formatFlag[0]!.split("=")[1] : 'cjs';
+const format = formatFlag.length >=1 ? formatFlag[0]!.split("=")[1] as Format : 'cjs';
   
 const files = glob.sync(fileDir);
   
