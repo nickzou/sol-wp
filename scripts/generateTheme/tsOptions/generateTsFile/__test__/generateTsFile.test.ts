a267122443599063e6f8fc30d4be4017e320149c
Nick Zou <nick@elevate.ca>
2023-10-02 17:36:21 -0400

    add generateTsFile test

---
+++ scripts/generateTheme/tsOptions/generateTsFile/__test__/generateTsFile.test.ts
@@ -0,0 +1,20 @@
+import generateTsFile from "../generateTsFile";
+
+describe("generateTsFile", () => {
+  it("should generate a correct ts file content", () => {
+    const themeName = "dark";
+
+    const result = generateTsFile({ themeName });
+
+    expect(result.name).toBe("index.ts");
+    expect(result.content).toBe(`console.log("hello dark");`);
+  });
+
+  it("should insert the themeName correctly into the content", () => {
+    const themeName = "light";
+
+    const result = generateTsFile({ themeName });
+
+    expect(result.content).toBe(`console.log("hello light");`);
+  });
+});
