66945b20275713404f419a492b235512fe11fe2d
Nick Zou <nick@elevate.ca>
2023-10-02 20:59:11 -0400

    add camelCase function

---
+++ scripts/utils/camelCase/camelCase.ts
@@ -0,0 +1,12 @@
+const camelCase = (str: string): string => {
+  const words = str.split(/[\s_-]+/);
+
+  return words
+    .map((word, index) => {
+      if (index === 0) {
+        return word.toLowerCase();
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join("");
+};
