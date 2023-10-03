1320a901b6c43076eff6a0166d77d24348e91569
Nick Zou <nick@elevate.ca>
2023-10-02 21:24:55 -0400

    create generateComposerFIle

---
+++ scripts/generateTheme/generateComposerFile/generateComposerFile.ts
@@ -0,0 +1,34 @@
+import { File } from "@utils/types/File";
+import camelCase from "@utils/camelCase/camelCase";
+
+interface generateComposerFile {
+  themeFolder: string;
+}
+
+const generateComposerFile = ({ themeFolder }: generateComposerFile): File => {
+  const content = `{"name": "nickzou/${themeFolder}}",
+  "description": "A modern toolchain for WordPress development.",
+  "type": "library",
+  "license": "MIT",
+  "autoload": {
+      "psr-4": {
+          "wpSol\\\\${camelCase(themeFolder)}\\\\": "./"
+      }
+  },
+  "authors": [
+      {
+          "name": "Nick Zou",
+          "email": "contact@nickzou.com"
+      }
+  ],
+  "minimum-stability": "dev",
+  "require": {}
+}`;
+
+  return {
+    name: "composer.json",
+    content,
+  };
+};
+
+export default generateComposerFile;
