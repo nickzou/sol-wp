ec2f624b5c7a9646cf1019ec877407d15893ad2d
Nick Zou <z.nick00@gmail.com>
2023-10-23 22:36:34 -0400

    update index.latte template

--- scripts/generateTheme/phpOptions/latte/generateIndexLatteTemplateFile/generateIndexLatteTemplateFile.ts
+++ scripts/generateTheme/phpOptions/latte/generateIndexLatteTemplateFile/generateIndexLatteTemplateFile.ts
@@ -1,7 +1,39 @@
 import { File } from "@utils/types/File";
 
 const generateIndexLatteTemplateFile = ():File => {
-    const content = `<main>this is latte</main>`;
+    const content = `<!DOCTYPE html>
+    <html {$language_attributes}>
+    <head>
+        <meta charset="{$charset}">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0\">
+        <title>{$title}</title>
+        {php wp_head()}
+    </head>
+    <body class="{$body_class|join:' '}">
+        <header>
+            <h1>{{site_name}}</h1>
+            <p>{{site_description}}</p>
+        </header>
+        <main>
+            { if $posts }
+                { for $posts as $post }
+                    <article>
+                        <h2><a href="{$post.link}">{$post.title}</a></h2>
+                        {$post.content}
+                    </article>
+                {% endfor %}
+            { else }
+                No posts found.
+            { endif }
+        </main>
+        
+        <footer>
+            <p>&copy; {"now"|date:'Y'} {$site_name}</p>
+        </footer>
+        
+        {php wp_footer()}
+    </body>
+    </html>`;
 
     return {
         name: 'index.latte',
