36c3d0e6b0576678f21bf1d64058a884d391a54f
Nick Zou <z.nick00@gmail.com>
2023-10-24 13:08:00 -0400

    add getAnswers function to encapsulate the IO portion

---
+++ scripts/generateTheme/getAnswers/getAnswers.ts
@@ -0,0 +1,121 @@
+import { Recipe } from "@utils/types/Recipe";
+import { text, select, confirm, isCancel } from "@clack/prompts";
+import formatFolderName from "@utils/formatFolderName/formatFolderName";
+import cssOptions from "@utils/vars/cssOptions";
+import phpOptions from "@utils/vars/phpOptions";
+
+const getAnswers = async ():Promise<Recipe> => {
+    const htmlRegex = /<\/?[a-z][\s\S]*>/i;
+    const spacesRegex = /\s+/;
+
+    const getName = await text({
+        message: `Enter a name for your theme.`,
+        placeholder: `Sol WP`,
+        initialValue: ``,
+        validate(value) {
+          if (htmlRegex.test(value)) return `Theme Name cannot contain HTML`;
+        },
+    });
+
+    if (isCancel(getName)) {
+        process.exit(0);
+    }
+
+    const getDirectory = await text({
+        message: `Enter theme folder name.`,
+        placeholder: `leave blank and we'll generate one based off your theme name`,
+        initialValue: ``,
+        validate(value) {
+          if (htmlRegex.test(value)) return `Folder Name cannot contain HTML`;
+          if (spacesRegex.test(value)) return `Folder Name cannot contain spaces`;
+        },
+    });
+      
+    if (isCancel(getDirectory)) {
+        process.exit(0);
+    }
+
+    const getAuthor = await text({
+        message: `Provide an author (optional)`,
+        placeholder: `Your name here`,
+    });
+      
+    if (isCancel(getAuthor)) {
+        process.exit(0);
+    }
+
+    const getDescription = await text({
+        message: `Provide a description (optional)`,
+        placeholder: `Your theme description here`,
+    });
+      
+    if (isCancel(getDescription)) {
+        process.exit(0);
+    }
+
+    const getVersion = await text({
+        message: `Provide a version (optional):`,
+        placeholder: `1.0.0`,
+    });
+      
+    if (isCancel(getVersion)) {
+        process.exit(0);
+    }
+
+    const cssOption = await select({
+        message: `What CSS tools would you like?`,
+        options: [
+          { value: "tailwind", label: "Tailwind" },
+          { value: "uno", label: "UnoCSS" },
+          { value: "sass", label: "Sass" },
+          { value: "postcss", label: "PostCSS" },
+          { value: "none", label: "None, I'll figure it out my own damn self." },
+        ],
+    });
+      
+    if (isCancel(cssOption)) {
+        process.exit(0);
+    }
+
+    const ts = await confirm({
+        message: `Would you to use TypeScript?`,
+    });
+      
+    if (isCancel(ts)) {
+        process.exit(0);
+    }
+
+    const phpOption = await select({
+        message: "What PHP Templating System would you like?",
+        options: [
+          { value: "twig", label: "Twig" },
+          { value: "latte", label: "Latte" },
+          { value: "bladeone", label: "BladeOne" },
+          { value: "plate", label: "Plate" },
+          { value: "smarty", label: "Smarty" },
+          { value: "mustache", label: "Mustache" },
+          { value: "none", label: "None, PHP is a templating engine!" },
+        ],
+    });
+      
+    if (isCancel(phpOption)) {
+        process.exit(0);
+    }
+
+    return {
+        theme: {
+            name: getName as string ?? 'Sol WP',
+            folder: getDirectory ? formatFolderName(getDirectory) ? formatFolderName(getName),
+            author: getAuthor as string ?? 'Sol WPer',
+            description: getDescription as string ?? 'Theme Description',
+            version: getVersion as string ?? '1.0.0',
+        },
+        tooling: {
+            css: cssOptions.filter((o) => o.name === cssOption)[0],
+            ts: ts,
+            php: phpOptions.filter((o) => o.name === phpOption)[0]
+        }
+    }
+};
+
+export default getAnswers;
