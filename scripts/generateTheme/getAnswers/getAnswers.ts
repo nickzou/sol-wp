9882bffc6425a1bf988ac62ad0cd2c7b98bc8d1b
Nick Zou <z.nick00@gmail.com>
2023-11-22 23:21:54 -0500

    update import

--- scripts/generateTheme/getAnswers/getAnswers.ts
+++ scripts/generateTheme/getAnswers/getAnswers.ts
@@ -1,6 +1,6 @@
 import { Recipe } from "@utils/types/Recipe";
 import { text, select, confirm, isCancel } from "@clack/prompts";
-import formatFolderName from "@utils/formatFolderName/formatFolderName";
+import formatFolderName from "@utils/formatDirectoryName/formatDirectoryName";
 import cssOptions from "@utils/vars/cssOptions";
 import phpOptions from "@utils/vars/phpOptions";
 
