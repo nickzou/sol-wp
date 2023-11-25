22388937c89359125bd06aea00b61015f225043b
Nick Zou <z.nick00@gmail.com>
2023-11-24 22:10:18 -0500

    update spacing

--- scripts/generateTheme/cssOptions/uno/setupUno/setupUno.ts
+++ scripts/generateTheme/cssOptions/uno/setupUno/setupUno.ts
@@ -5,34 +5,34 @@ import createFile from "@utils/createFile/createFile";
 import { SetupCss } from "@utils/types/SetupCss";
 
 const setupUno = async ({functionFile, answers, npmPackages, packageScripts}:SetupCss) => {
-      await styleSolutionEnqueuer({
-        functionFile,
-        theme: answers.theme,
-        option: answers.tooling.css,
-      });
+  await styleSolutionEnqueuer({
+    functionFile,
+    theme: answers.theme,
+    option: answers.tooling.css,
+  });
 
-      let tailwindAndUnoContent = generateTailwindAndUnoContent(answers);
+  let tailwindAndUnoContent = generateTailwindAndUnoContent(answers);
 
-      packageScripts.push(...[
-        { key: "uno", value: "unocss" },
-        { key: "uno:prod", value: "unocss --minify" },
-        { key: "uno:watch", value: "unocss --watch" },
-      ]);
+  packageScripts.push(...[
+    { key: "uno", value: "unocss" },
+    { key: "uno:prod", value: "unocss --minify" },
+    { key: "uno:watch", value: "unocss --watch" },
+  ]);
 
-      const unoConfigFile = generateUnoConfigFile({
-        content: tailwindAndUnoContent,
-        outFile: `wp/themes/${answers.theme.directory}/css/uno.css`,
-      });
+  const unoConfigFile = generateUnoConfigFile({
+    content: tailwindAndUnoContent,
+    outFile: `wp/themes/${answers.theme.directory}/css/uno.css`,
+  });
 
-      createFile({
-        directoryPath: ".",
-        fileName: unoConfigFile.name,
-        fileContent: unoConfigFile.content,
-      });
+  createFile({
+    directoryPath: ".",
+    fileName: unoConfigFile.name,
+    fileContent: unoConfigFile.content,
+  });
 
-      npmPackages.push(...[
-        `${answers.tooling.css.packageName}`,
-      ]);
+  npmPackages.push(...[
+    `${answers.tooling.css.packageName}`,
+  ]);
 };
 
 export default setupUno;
