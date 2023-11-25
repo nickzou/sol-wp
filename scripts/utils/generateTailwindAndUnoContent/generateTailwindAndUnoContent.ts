5088da0887b6cc9b109c0aa76d54e40ee439c02a
Nick Zou <z.nick00@gmail.com>
2023-11-25 02:22:07 -0500

    refactor tailwind and uno content to use extensions

--- scripts/utils/generateTailwindAndUnoContent/generateTailwindAndUnoContent.ts
+++ scripts/utils/generateTailwindAndUnoContent/generateTailwindAndUnoContent.ts
@@ -5,15 +5,10 @@ const generateTailwindAndUnoContent = (answers:Recipe) => {
 
     let content = [
         `wp/themes/${theme.directory}/**/*.php`,
-        `src/themes/${theme.directory}/ts/**/*.{js, jsx, ts, tsx}`
+        `src/themes/${theme.directory}/ts/**/*.{js, jsx, ts, tsx}`,
+        `wp/themes/${theme.directory}/views/**/*.${tooling.template.extension}`
     ];
-
-    if(tooling.template.name === 'twig') {
-        content.push(`wp/themes/${theme.directory}/views/**/*.twig`);
-    } else if (tooling.template.name === 'latte') {
-        content.push(`wp/themes/${theme.directory}/views/**/*.latte`);
-    }
-    
+ 
     return content;
 };
 
