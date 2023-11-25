2f6c06808d873ed963fb370ed40e39517befda32
Nick Zou <z.nick00@gmail.com>
2023-11-24 20:49:47 -0500

    add setupCss strategy function

---
+++ scripts/generateTheme/cssOptions/setupCss/setupCss.ts
@@ -0,0 +1,31 @@
+import formatMessage from "@utils/formatMessage/formatMessage";
+import setupPostCss from "../postcss/setupPostCss/setupPostCss";
+import setupSass from "../sass/setupSass/setupSass";
+import setupTailwind from "../tailwind/setupTailwind/setupTailwind";
+import setupUno from "../uno/setupUno/setupUno";
+import { SetupCss } from "@utils/types/SetupCss";
+
+const setupCss = async ({functionFile, answers}:SetupCss) => {
+    const cssOptions = {
+        tailwind: setupTailwind,
+        uno: setupUno,
+        sass: setupSass,
+        postCss: setupPostCss,
+        none: async ({functionFile, answers}:SetupCss) => {
+            console.log(formatMessage({message: `Alright, good luck!`, color: 'yellow'}))
+        }
+    };
+
+    try {
+        const optionName = answers.tooling.css.name;
+        const optionFunction = cssOptions[optionName];
+
+        if(optionFunction) {
+            await optionFunction({functionFile, answers});
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default setupCss;
