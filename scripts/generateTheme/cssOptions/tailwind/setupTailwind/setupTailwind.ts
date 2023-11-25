95dccae482f4784e9ae1a233da18e414248b1685
Nick Zou <z.nick00@gmail.com>
2023-11-24 22:09:50 -0500

    update spacing

--- scripts/generateTheme/cssOptions/tailwind/setupTailwind/setupTailwind.ts
+++ scripts/generateTheme/cssOptions/tailwind/setupTailwind/setupTailwind.ts
@@ -7,61 +7,61 @@ import generateTailwindCssFile from "../generateTailwindCssFile/generateTailwind
 import { SetupCss } from "@utils/types/SetupCss";
 
 const setupTailwind = async ({functionFile, answers, npmPackages, packageScripts, prettierConfigOptions}:SetupCss) => {
-      await styleSolutionEnqueuer({
-        functionFile,
-        theme: answers.theme,
-        option: answers.tooling.css,
-      });
+  await styleSolutionEnqueuer({
+    functionFile,
+    theme: answers.theme,
+    option: answers.tooling.css,
+  });
 
-      let tailwindAndUnoContent = generateTailwindAndUnoContent(answers);
+  let tailwindAndUnoContent = generateTailwindAndUnoContent(answers);
 
-      createDirectory({
-        location: `src/themes/${answers.theme.directory}`,
-        directoryName: `css`,
-      });
+  createDirectory({
+    location: `src/themes/${answers.theme.directory}`,
+    directoryName: `css`,
+  });
 
-      packageScripts.push(...[
-        {
-          key: "tailwind",
-          value: `tailwindcss -i ./src/themes/${answers.theme.directory}/css/tailwind.css -o ./wp/themes/${answers.theme.directory}/css/tailwind.css`,
-        },
-        {
-          key: "tailwind:prod",
-          value: `tailwindcss -i ./src/themes/${answers.theme.directory}/css/tailwind.css -o ./wp/themes/${answers.theme.directory}/css/tailwind.css`,
-        },
-        {
-          key: "tailwind:watch",
-          value: `tailwindcss -i ./src/themes/${answers.theme.directory}/css/tailwind.css -o ./wp/themes/${answers.theme.directory}/css/tailwind.css --watch`,
-        },
-      ]);
+  packageScripts.push(...[
+    {
+      key: "tailwind",
+      value: `tailwindcss -i ./src/themes/${answers.theme.directory}/css/tailwind.css -o ./wp/themes/${answers.theme.directory}/css/tailwind.css`,
+    },
+    {
+      key: "tailwind:prod",
+      value: `tailwindcss -i ./src/themes/${answers.theme.directory}/css/tailwind.css -o ./wp/themes/${answers.theme.directory}/css/tailwind.css`,
+    },
+    {
+      key: "tailwind:watch",
+      value: `tailwindcss -i ./src/themes/${answers.theme.directory}/css/tailwind.css -o ./wp/themes/${answers.theme.directory}/css/tailwind.css --watch`,
+    },
+  ]);
 
-      const tailwindConfigFile = generateTailwindConfigFile({
-        content: tailwindAndUnoContent,
-      });
+  const tailwindConfigFile = generateTailwindConfigFile({
+    content: tailwindAndUnoContent,
+  });
 
-      const tailwindCssFile = generateTailwindCssFile();
+  const tailwindCssFile = generateTailwindCssFile();
 
-      createFile({
-        directoryPath: `.`,
-        fileName: tailwindConfigFile.name,
-        fileContent: tailwindConfigFile.content,
-      });
+  createFile({
+    directoryPath: `.`,
+    fileName: tailwindConfigFile.name,
+    fileContent: tailwindConfigFile.content,
+  });
 
-      createFile({
-        directoryPath: `src/themes/${answers.theme.directory}/css`,
-        fileName: tailwindCssFile.name,
-        fileContent: tailwindCssFile.content,
-      });
+  createFile({
+    directoryPath: `src/themes/${answers.theme.directory}/css`,
+    fileName: tailwindCssFile.name,
+    fileContent: tailwindCssFile.content,
+  });
 
-      prettierConfigOptions.plugins.push(...[
-        "prettier-plugin-tailwindcss",
-      ]);
+  prettierConfigOptions.plugins.push(...[
+    "prettier-plugin-tailwindcss",
+  ]);
 
-      npmPackages.push(...[
-        `tailwindcss`,
-        "prettier",
-        "prettier-plugin-tailwindcss",
-      ]);
+  npmPackages.push(...[
+    `tailwindcss`,
+    "prettier",
+    "prettier-plugin-tailwindcss",
+  ]);
 };
 
 export default setupTailwind;
