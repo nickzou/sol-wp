a044c05d09c025c70ff99efd729390129bc6ed8b
Nick Zou <z.nick00@gmail.com>
2024-01-03 23:48:36 -0500

    update global declarations

--- scripts/createTheme/templateOptions/bladeone/generateSetupBladeOnePhpFunctionFile/generateSetupBladeOnePhpFunctionFile.ts
+++ scripts/createTheme/templateOptions/bladeone/generateSetupBladeOnePhpFunctionFile/generateSetupBladeOnePhpFunctionFile.ts
@@ -4,9 +4,12 @@ const generateSetupBladeOneFunctionFile = () => {
     return generatePhpFunctionFile({
         name: "setup_bladeone",
         functionBody: ` global $twig, $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
-        global $views = __DIR__ . '/views';
-        global $cache = __DIR__ . '/cache';
-        global $blade = new eftec\\bladeone\\BladeOne($views,$cache);`
+        $views = __DIR__ . '/views';
+        $cache = __DIR__ . '/cache';
+        $blade = new eftec\\bladeone\\BladeOne($views,$cache);
+        global $views;
+        global $cache;
+        global $blade;`
     })
 };
 
