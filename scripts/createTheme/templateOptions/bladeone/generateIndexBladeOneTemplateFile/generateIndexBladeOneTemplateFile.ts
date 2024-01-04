2e5b28a06720260ae81d7f10c69e4cb49cc0b108
Nick Zou <z.nick00@gmail.com>
2024-01-03 23:28:03 -0500

    add <html> opening tag

--- scripts/createTheme/templateOptions/bladeone/generateIndexBladeOneTemplateFile/generateIndexBladeOneTemplateFile.ts
+++ scripts/createTheme/templateOptions/bladeone/generateIndexBladeOneTemplateFile/generateIndexBladeOneTemplateFile.ts
@@ -2,9 +2,9 @@ import { File } from "@utils/types/File";
 
 const generateIndexBladeOneTemplateFile = ():File => {
     const content = `<!DOCTYPE html>
+<html>
     <p>hello world</p>
-    </html>
-    `;
+</html>`;
 
     return {
         name: 'index.blade.php',
