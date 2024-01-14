c424cc1370bd3d74ae64286e6e752c97dae5e389
Nick Zou <z.nick00@gmail.com>
2024-01-13 20:47:41 -0500

    refactor setupfile

--- scripts/createTheme/templateOptions/bladeone/generateSetupBladeOnePhpFunctionFile/generateSetupBladeOnePhpFunctionFile.ts
+++ scripts/createTheme/templateOptions/bladeone/generateSetupBladeOnePhpFunctionFile/generateSetupBladeOnePhpFunctionFile.ts
@@ -3,13 +3,12 @@ import generatePhpFunctionFile from "@createTheme/generatePhpFunctionFile/genera
 const generateSetupBladeOneFunctionFile = () => {
     return generatePhpFunctionFile({
         name: "setup_bladeone",
-        functionBody: ` global $twig, $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
-        $views = __DIR__ . '/views';
-        $cache = __DIR__ . '/cache';
-        $blade = new eftec\\bladeone\\BladeOne($views,$cache);
-        global $views;
-        global $cache;
-        global $blade;`
+        functionBody: `$views = get_template_directory() . '/views';
+  $cache = get_template_directory() . '/cache';
+  
+  global $blade, $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+
+  $blade = new eftec\bladeone\BladeOne($views, $cache);`
     })
 };
 
