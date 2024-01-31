c09edaf03ca91313dbeb97fcb4fa05bda6078940
Nick Zou <z.nick00@gmail.com>
2024-01-30 21:47:58 -0500

    add generateIndexPlatesFile function

---
+++ scripts/createTheme/templateOptions/plates/generateIndexPlatesFile/generateIndexPlatesFile.ts
@@ -0,0 +1,32 @@
+import { File } from "@utils/types/File";
+
+const generateIndexPlatesFile = ():File => {
+    const content = `<?php
+    $post_data = [];
+    
+    if( have_posts()){
+        while(have_posts()) {
+            the_post();
+
+            $post_data[] = [
+                'link' => get_the_permalink(),
+                'title' => get_the_title(),
+                'content' => get_the_content(),
+            ];
+        }
+    };
+    
+    $context = [
+        'posts => $post_data
+    ];
+    
+    echo $views->render('index', $context);
+?>`;
+
+    return {
+        name: 'index.php',
+        content
+    }
+};
+
+export default generateIndexPlatesFile;
