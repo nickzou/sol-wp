22197cbcf15be2cca83ade92eb560b8ee090936d
Nick Zou <z.nick00@gmail.com>
2024-01-30 22:06:26 -0500

    complete setupPlates

--- scripts/createTheme/templateOptions/plates/setupPlates/setupPlates.ts
+++ scripts/createTheme/templateOptions/plates/setupPlates/setupPlates.ts
@@ -3,6 +3,8 @@ import generateSetupPlatesFunctionFile from "../generateSetupPlatesFunctionFile/
 import createFile from "@utils/createFile/createFile";
 import createDirectory from "@utils/createDirectory/createDirectory";
 import appendToFunctionsFile from "@createTheme/appendToFunctionsFile/appendToFunctionsFile";
+import generateIndexPlatesFile from "../generateIndexPlatesFile/generateIndexPlatesFile";
+import generateIndexPlatesTemplateFile from "../generateIndexPlatesTemplateFile/generateIndexPlatesTemplateFile";
 
 const setupPlates = async ({answers, composerPackages}:SetupTemplate) => {
     composerPackages.push(...[
@@ -17,15 +19,31 @@ const setupPlates = async ({answers, composerPackages}:SetupTemplate) => {
         fileContent: `${setupPlatesFile.content} \nadd_action('template_redirect', 'setup_plates');`
     });
 
+    appendToFunctionsFile({
+        themeFolder: answers.theme.directory,
+        functionName: setupPlatesFile.functionName
+    });
+
     createDirectory({
         location: `wp/themes/${answers.theme.directory}`,
         directoryName: 'views'
     });
 
-    appendToFunctionsFile({
-        themeFolder: answers.theme.directory,
-        functionName: setupPlatesFile.functionName
+    const platesIndexFile = generateIndexPlatesFile();
+
+    createFile({
+        directoryPath: `wp/themes/${answers.theme.directory}`,
+        fileName: platesIndexFile.name,
+        fileContent: platesIndexFile.content,
     });
+
+    const platesIndexTemplateFile = generateIndexPlatesTemplateFile();
+
+    createFile({
+        directoryPath: `wp/themes/${answers.theme.directory}/views`,
+        fileName: platesIndexTemplateFile.name,
+        fileContent: platesIndexTemplateFile.content,
+    });   
 };
 
 export default setupPlates;
