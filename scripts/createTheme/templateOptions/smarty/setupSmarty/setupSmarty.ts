297a4d4423904a1c12e5bcc3c85974836423ef62
Nick Zou <z.nick00@gmail.com>
2024-01-31 19:42:20 -0500

    update setupSmarty with create smarty directories

--- scripts/createTheme/templateOptions/smarty/setupSmarty/setupSmarty.ts
+++ scripts/createTheme/templateOptions/smarty/setupSmarty/setupSmarty.ts
@@ -1,9 +1,37 @@
+import createDirectory from "@utils/createDirectory/createDirectory";
 import { SetupTemplate } from "@utils/types/SetupTemplate";
 
 const setupSmarty = async ({answers, composerPackages}:SetupTemplate) => {
     composerPackages.push(...[
         "smarty/smarty"
     ]);
+
+    //const setupSmartyFile = 
+
+    createDirectory({
+        location: `wp/themes/${answers.theme.directory}`,
+        directoryName: 'views'
+    });
+
+    createDirectory({
+        location: `wp/themes/${answers.theme.directory}/views`,
+        directoryName: 'views'
+    });
+
+    createDirectory({
+        location: `wp/themes/${answers.theme.directory}/views`,
+        directoryName: 'config'
+    });
+
+    createDirectory({
+        location: `wp/themes/${answers.theme.directory}/views`,
+        directoryName: 'compile'
+    });
+
+    createDirectory({
+        location: `wp/themes/${answers.theme.directory}/views`,
+        directoryName: 'cache'
+    });
 };
 
 export default setupSmarty;
