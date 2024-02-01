f74840048ee895db05f577609e0b64621f6cd202
Nick Zou <z.nick00@gmail.com>
2024-01-31 20:30:59 -0500

    add namespace to Smarty instantiation

--- scripts/createTheme/templateOptions/smarty/generateSetupSmartyFunctionFile/generateSetupSmartyFunctionFile.ts
+++ scripts/createTheme/templateOptions/smarty/generateSetupSmartyFunctionFile/generateSetupSmartyFunctionFile.ts
@@ -4,7 +4,7 @@ const generateSetupSmartyFunctionFile = () => {
     return generatePhpFunctionFile({
         name: "setup_smarty",
         functionBody: `  global $views, $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
-        $views = new Smarty();
+        $views = new \\Smarty\\Smarty();
         $views->setTemplateDir(get_template_directory() . '/views/views');
         $views->setConfigDir(get_template_directory() . '/views/config');
         $views->setCompileDir(get_template_directory() . '/views/compile');
