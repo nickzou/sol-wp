576043bd7202f80b2ed85c522e410c804365cb3f
Nick Zou <z.nick00@gmail.com>
2024-01-31 19:59:23 -0500

    update setupSmarty to add createFile

--- scripts/createTheme/templateOptions/smarty/setupSmarty/setupSmarty.ts
+++ scripts/createTheme/templateOptions/smarty/setupSmarty/setupSmarty.ts
@@ -1,12 +1,20 @@
 import createDirectory from "@utils/createDirectory/createDirectory";
 import { SetupTemplate } from "@utils/types/SetupTemplate";
+import generateSetupSmartyFunctionFile from "../generateSetupSmartyFunctionFile/generateSetupSmartyFunctionFile";
+import createFile from "@utils/createFile/createFile";
 
 const setupSmarty = async ({answers, composerPackages}:SetupTemplate) => {
     composerPackages.push(...[
         "smarty/smarty"
     ]);
 
-    //const setupSmartyFile = 
+    const setupSmartyFile = generateSetupSmartyFunctionFile();
+
+    createFile({
+        directoryPath: `wp/themes/${answers.theme.directory}/functions`,
+        fileName: setupSmartyFile.name,
+        fileContent: `${setupSmartyFile.content} \nadd_action('tempalte_redirect', 'setup_smarty');`
+    })
 
     createDirectory({
         location: `wp/themes/${answers.theme.directory}`,
