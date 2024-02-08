907000c98c9ba29ffdd0dafd09e842d757d22f5d
Nick Zou <z.nick00@gmail.com>
2024-02-07 19:31:26 -0500

    add Recipe type

--- scripts/utils/types/Recipe.ts
+++ scripts/utils/types/Recipe.ts
@@ -1,6 +1,7 @@
 import { Theme } from "./Theme";
 import { CssOption } from "./CssOption";
 import { TemplateOption } from "./TemplateOption";
+import { TestingOptions } from "./TestingOptions";
 
 export type Recipe = {
   theme: Theme;
@@ -8,5 +9,7 @@ export type Recipe = {
     css: CssOption;
     ts: boolean;
     template: TemplateOption;
+    testing: boolean;
+    testingOptions: TestingOptions;
   };
 };
