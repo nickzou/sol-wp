681e19fd302c27d0c3a799f30a0cbaf0448ca161
Nick Zou <z.nick00@gmail.com>
2024-02-08 17:37:49 -0500

    fix spread error

--- scripts/createTheme/testingOptions/setupTestingOptions/setupTestingOptions.ts
+++ scripts/createTheme/testingOptions/setupTestingOptions/setupTestingOptions.ts
@@ -4,7 +4,7 @@ const setupTestingOptions = async ({answers, composerPackages}:SetupTesting) => 
     if(answers.tooling.testing) {
         Object.keys(answers.tooling.testingOptions).forEach(key => {
             if (answers.tooling.testingOptions[key]) {
-                composerPackages.push(...testingOptions.filter(option => option.name === key)[0].packageName);
+                composerPackages.push(testingOptions.filter(option => option.name === key)[0].packageName);
             }
         });
     }
