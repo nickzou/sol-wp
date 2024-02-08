fecce6e9001ea7420ad817ffeb48af2504d24baf
Nick Zou <z.nick00@gmail.com>
2024-02-07 21:03:54 -0500

    add testingOptions

---
+++ scripts/createTheme/testingOptions/setupTestingOptions/setupTestingOptions.ts
@@ -0,0 +1,6 @@
+import { SetupPhpPackages as SetupTesting } from "@utils/types/SetupPhpPackages"; "@utils/types/SetupPhpPackages";
+const setupTestingOptions = async ({answers, composerPackages}:SetupTesting) => {
+    
+};
+
+export default setupTestingOptions;
---
+++ scripts/utils/vars/testingOptions.ts
@@ -0,0 +1,7 @@
+import { TestingOption } from "@utils/types/TestingOption";
+
+const testingOptions: TestingOption[] = [
+    { name: "phpunit", packageName: "phpunit/phpunit "}
+];
+
+export default testingOptions;
