f121272a6bfb0ee45abe6aa7bff0efba48c4034e
Nick Zou <z.nick00@gmail.com>
2024-02-09 19:52:46 -0500

    add WP_Mock package

--- scripts/utils/vars/testingOptions.ts
+++ scripts/utils/vars/testingOptions.ts
@@ -1,7 +1,13 @@
 import { TestingOption } from "@utils/types/TestingOption";
 
 const testingOptions: TestingOption[] = [
-    { name: "phpunit", packageName: ["phpunit/phpunit"], language: 'php' }
+    { 
+        name: "phpunit",
+        packageName: [
+            "phpunit/phpunit",
+            "10up/wp_mock"
+        ],
+        language: 'php' }
 ];
 
 export default testingOptions;
