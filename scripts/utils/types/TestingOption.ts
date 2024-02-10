0e59697f4c4abadcb9d068ed351b305eab62fc37
Nick Zou <z.nick00@gmail.com>
2024-02-09 19:49:54 -0500

    update packageName types

--- scripts/utils/types/TestingOption.ts
+++ scripts/utils/types/TestingOption.ts
@@ -1,5 +1,5 @@
 export type TestingOption = {
     name: string;
-    packageName: string;
+    packageName: string[];
     language: 'javascript' | 'php';
 };
--- scripts/utils/vars/testingOptions.ts
+++ scripts/utils/vars/testingOptions.ts
@@ -1,7 +1,7 @@
 import { TestingOption } from "@utils/types/TestingOption";
 
 const testingOptions: TestingOption[] = [
-    { name: "phpunit", packageName: "phpunit/phpunit", language: 'php' }
+    { name: "phpunit", packageName: ["phpunit/phpunit"], language: 'php' }
 ];
 
 export default testingOptions;
