bbc63d0aafa4663bf8671777e4dabd7efebff1eb
Nick Zou <z.nick00@gmail.com>
2024-02-13 20:23:43 -0500

    change generatePhpUnitXml interface to add bootstrap parameter

--- scripts/createTheme/testingOptions/common/generatePhpUnitXml/generatePhpUnitXml.ts
+++ scripts/createTheme/testingOptions/common/generatePhpUnitXml/generatePhpUnitXml.ts
@@ -2,11 +2,12 @@ import { File } from "@utils/types/File"
 
 type GeneratePhpUnitXml = {
     themeName: string;
+    bootstrap?: string;
 }
 
-const generatePhpUnitXml = ({themeName}:GeneratePhpUnitXml):File => {
+const generatePhpUnitXml = ({themeName, bootstrap = 'vendor/autoload.php' }:GeneratePhpUnitXml):File => {
     const content = `<?xml version="1.0"?>
-<phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" bootstrap="vendor/autoload.php" colors="true" xsi:noNamespaceSchemaLocation="https://schema.phpunit.de/11.1/phpunit.xsd" cacheDirectory=".phpunit.cache">
+<phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" bootstrap="${bootstrap}" colors="true" xsi:noNamespaceSchemaLocation="https://schema.phpunit.de/11.1/phpunit.xsd" cacheDirectory=".phpunit.cache">
   <testsuites>
     <testsuite name="${themeName} Theme Tests">
       <directory>tests</directory>
