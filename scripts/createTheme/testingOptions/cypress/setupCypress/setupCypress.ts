e6383f8ee4a4526627dd57889adce39598e094c6
Nick Zou <z.nick00@gmail.com>
2024-02-19 14:50:03 -0500

    add configuration template for playwright

--- scripts/createTheme/testingOptions/cypress/setupCypress/setupCypress.ts
+++ scripts/createTheme/testingOptions/cypress/setupCypress/setupCypress.ts
@@ -2,6 +2,7 @@ import { SetupTestingOption } from "@utils/types/SetupTestingOption";
 import testingOptions from "@utils/vars/testingOptions";
 import generateCypressConfig from "../generateCypressConfig/generateCypressConfig";
 import createFile from "@utils/createFile/createFile";
+import createDirectory from "@utils/createDirectory/createDirectory";
 
 const setupCypress = async({answers, packages, packageScripts}:SetupTestingOption) => {
     const option = testingOptions.filter(o => o.name === 'cypress')[0];
@@ -13,6 +14,11 @@ const setupCypress = async({answers, packages, packageScripts}:SetupTestingOptio
         directoryPath: '.',
         fileName: cypressConfigFile.name,
         fileContent: cypressConfigFile.content
+    });
+
+    createDirectory({
+        location: '.',
+        directoryName: 'cypress'
     });
 
     packageScripts.push(...[{
