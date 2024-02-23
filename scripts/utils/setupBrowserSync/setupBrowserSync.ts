b99a1b04f5f41544ec7bf85499fbc03b3150f3b4
Nick Zou <z.nick00@gmail.com>
2024-02-22 21:15:35 -0500

    add setupBrowserSync

---
+++ scripts/utils/setupBrowserSync/setupBrowserSync.ts
@@ -0,0 +1,18 @@
+import { Recipe } from "@utils/types/Recipe";
+import { PackageScript } from "@utils/vars/packageScripts";
+
+type SetupBrowserSync = {
+    npmPackages: string[];
+    packageScripts: PackageScript[];
+};
+
+const setupBrowserSync = async ({npmPackages, packageScripts}:SetupBrowserSync) => {
+    npmPackages.push(...["browser-sync"]);
+
+    packageScripts.push(...[{
+        key: "browsersync",
+        value: "browser-sync --proxy \"localhost:8888\" --files \"wp/**/*\""
+    }]);
+};
+
+export default setupBrowserSync;
