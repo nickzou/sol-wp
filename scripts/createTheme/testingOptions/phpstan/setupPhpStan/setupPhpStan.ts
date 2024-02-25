021d53b9efa6afb7d55df044544f6766f345af9c
Nick Zou <z.nick00@gmail.com>
2024-02-25 14:54:14 -0500

    add config and bootstrap files to setupPhpStan

--- scripts/createTheme/testingOptions/phpstan/setupPhpStan/setupPhpStan.ts
+++ scripts/createTheme/testingOptions/phpstan/setupPhpStan/setupPhpStan.ts
@@ -1,9 +1,34 @@
 import { SetupTestingOption } from "@utils/types/SetupTestingOption";
 import testingOptions from "@utils/vars/testingOptions";
+import generatePhpStanNeon from "../generatePhpStanNeonFile/generatePhpStanNeon";
+import createFile from "@utils/createFile/createFile";
+import createDirectory from "@utils/createDirectory/createDirectory";
+import generatePhpStanBootstrapFile from "../generatePhpStanBootstrapFile/generatePhpStanBootstrapFile";
 
 const setupPhpStan = async ({answers, packages, packageScripts}:SetupTestingOption) => {
     const option = testingOptions.filter(o => o.name === 'phpstan')[0];
     packages.push(...option.packageName);
+
+    const phpStanNeonFile = generatePhpStanNeon(answers.theme.directory);
+
+    createFile({
+        directoryPath: '.',
+        fileName: phpStanNeonFile.name,
+        fileContent: phpStanNeonFile.content
+    });
+
+    createDirectory({
+        location: '.',
+        directoryName: 'phpstan'
+    });
+
+    const phpStanBootstrapFile = generatePhpStanBootstrapFile();
+
+    createFile({
+        directoryPath: './phpstan',
+        fileName: phpStanBootstrapFile.name,
+        fileContent: phpStanBootstrapFile.content
+    });
 
     packageScripts.push({
         key: 'phpstan',
