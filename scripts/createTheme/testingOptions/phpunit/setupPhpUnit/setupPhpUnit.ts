414c78627e1a1e599656e83dddea2609e355aa7e
Nick Zou <z.nick00@gmail.com>
2024-03-19 21:59:08 -0400

    update formatting

--- scripts/createTheme/testingOptions/phpunit/setupPhpUnit/setupPhpUnit.ts
+++ scripts/createTheme/testingOptions/phpunit/setupPhpUnit/setupPhpUnit.ts
@@ -5,34 +5,34 @@ import generatePhpUnitXml from "../../common/generatePhpUnitXml/generatePhpUnitX
 import { SetupTestingOption } from "@utils/types/SetupTestingOption";
 
 const setupPhpUnit = async ({answers, packages, packageScripts, watchScripts}:SetupTestingOption) => {
-    const option = testingOptions.filter(o => o.name === 'phpunit')[0];
-    packages.push(...option.packageName);
+  const option = testingOptions.filter(o => o.name === "phpunit")[0];
+  packages.push(...option.packageName);
 
-    createDirectory({
-        location: `wp/themes/${answers.theme.directory}`,
-        directoryName: 'tests'
-    });
+  createDirectory({
+    location: `wp/themes/${answers.theme.directory}`,
+    directoryName: "tests"
+  });
 
-    const phpUnitXml = generatePhpUnitXml({themeName: answers.theme.name});
+  const phpUnitXml = generatePhpUnitXml({themeName: answers.theme.name});
 
-    createFile({
-        directoryPath: `wp/themes/${answers.theme.directory}`,
-        fileName: phpUnitXml.name,
-        fileContent: phpUnitXml.content
-    });
+  createFile({
+    directoryPath: `wp/themes/${answers.theme.directory}`,
+    fileName: phpUnitXml.name,
+    fileContent: phpUnitXml.content
+  });
 
-    packageScripts.push(
-        {
-            key: 'test:phpunit',
-            value: `wp-env run tests-cli --env-cwd=wp-content/themes/${answers.theme.directory} ./vendor/bin/phpunit`
-        },
-        {
-            key: 'test:phpunit:watch',
-            value: `onchange "wp/themes/${answers.theme.directory}/**/*.php" -- npm run test:phpunit`
-        }
-    );
+  packageScripts.push(
+    {
+      key: "test:phpunit",
+      value: `wp-env run tests-cli --env-cwd=wp-content/themes/${answers.theme.directory} ./vendor/bin/phpunit`
+    },
+    {
+      key: "test:phpunit:watch",
+      value: `onchange "wp/themes/${answers.theme.directory}/**/*.php" -- npm run test:phpunit`
+    }
+  );
 
-    watchScripts.push("'npm run test:phpunit:watch'");
+  watchScripts.push("'npm run test:phpunit:watch'");
 };
 
 export default setupPhpUnit;
